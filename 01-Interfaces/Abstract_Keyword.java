0676464edc34ef89308af876221d2d41f0e6baab
Piyush Gangwar <piyushgangwar7668@gmail.com>
2026-04-24 22:38:11 +0530

    Inner Class

--- 01-Interfaces/Abstract_Keyword.java
+++ 01-Interfaces/Abstract_Keyword.java
@@ -1,8 +1,17 @@
-package Interfaces;
+public abstract class Abstract_Keyword {
+/*  agr hume koi function create krna h aur usko use nhi krna then wo abstract ban jata h. 
+    --> you cant create obj of abstract class.
+    --> class which have all method of abstract that class called concrete class.
 
-public class Abstract_Keyword {
+*/
 
+    // you also have to make class and method abstract to use abstrat keyword.
+    
+    public abstract void car(){
+
+    }
     public static void main(String[] args) {
+        System.out.println("hello world this is new word");
         
     }
 }
